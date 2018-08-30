d7fee0c7da66646afc6a358247f9d76f00ff387b
Cartrabbit <cartrabbit@Cartrabbits-iMac.local>
2018-08-30 11:36:20 +0530

    openform issue 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import grapesjs from 'grapesjs';
-import 'grapesjs-mjml';
-import 'grapesjs-campaign';
 
 module.exports = (() => {
   const defaultConfig = {
     fromElement: true,
-    plugins: ['gjs-mjml','gjs-campaign'],
-    pluginOpts: {
-      'gjs-mjml': {
-        resetDevices: 0
-      },
-      'gjs-campaign':{}
-    }
   }
   return {
     init( config = {} ) {
