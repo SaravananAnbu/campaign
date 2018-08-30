887aa47835e47fc69dd1e29c2fd97c173a061376
Cartrabbit <cartrabbit@Cartrabbits-iMac.local>
2018-08-30 16:45:42 +0530

    added plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import grapesjs from 'grapesjs';
 module.exports = (() => {
   const defaultConfig = {
     fromElement: true,
+    plugins: ['gjs-mjml', 'gjs-campaign'],
+    pluginsOpts: {
+      'gjs-mjml': {
+        resetDevices: 0
+      },
+      'gjs-campaign': {}
+    }
   }
   return {
     init( config = {} ) {
