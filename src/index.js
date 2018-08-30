5e9ec0f7b3281f0077137711e63dde048d0a26f0
Cartrabbit <cartrabbit@Cartrabbits-iMac.local>
2018-08-30 11:02:48 +0530

    Added Plugins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import grapesjs from 'grapesjs';
+import 'grapesjs-mjml';
+import 'grapesjs-campaign';
 
 module.exports = (() => {
+  const defaultConfig = {
+    fromElement: true,
+    plugins: ['gjs-mjml','gjs-campaign'],
+    pluginOpts: {
+      'gjs-mjml': {},
+      'gjs-campaign':{}
+    }
+  }
   return {
     init( config = {} ) {
-      config = { ...config }
+      config = { ...defaultConfig, ...config }
       console.log('config', config);
       const editor = grapesjs.init(config)
       return editor;
