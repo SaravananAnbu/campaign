4ef514e9be29e4549e39d13cd8ef4a516a02cd03
Cartrabbit <cartrabbit@Cartrabbits-iMac.local>
2018-08-30 13:29:24 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import grapesjs from 'grapesjs';
 module.exports = (() => {
   const defaultConfig = {
     fromElement: true,
-    autorender:1
   }
   return {
     init( config = {} ) {
       config = { ...defaultConfig, ...config }
       console.log('config', config);
-      const editor = grapesjs.init(config);
-      editor.getModel().loadOnStart();
-      config.autorender && editor.render();
+      const editor = grapesjs.init(config)
       return editor;
     }
   };
