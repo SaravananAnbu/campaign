732d945aaf332e36fc162e55bab2e26fbf004b1b
Cartrabbit <cartrabbit@Cartrabbits-iMac.local>
2018-08-30 13:25:23 +0530

    Render getModal

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import grapesjs from 'grapesjs';
 module.exports = (() => {
   const defaultConfig = {
     fromElement: true,
+    autorender:1
   }
   return {
     init( config = {} ) {
       config = { ...defaultConfig, ...config }
       console.log('config', config);
-      const editor = grapesjs.init(config)
+      const editor = grapesjs.init(config);
+      editor.getModel().loadOnStart();
+      config.autorender && editor.render();
       return editor;
     }
   };
